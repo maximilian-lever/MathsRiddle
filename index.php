02e0919257ac19c1472bf76f19371e09379294b7
maximilian-lever <maximilian.lever@gmail.com>
2019-04-22 15:59:51 +0100

    INITIAL PUSH

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include('./vendors/php/conjson.php') ?>
+<!doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+			<link rel="stylesheet" href="./resources/CSS/style.css" type="text/css" media="screen">
+			<title>Math Riddles</title>
+		</head>
+		<body>
+			<h1>Math Riddles</h1>
+			<?php '<div>'.$riddle->question.'</div>' ?>
+		</body>
+	</html>
---
+++ vendors/php/conjson.php
@@ -0,0 +1,5 @@
+<?php
+ $riddleJson = 'data/riddle.json';
+
+ $riddle = json_decode(file_get_contents($riddleJson))
+?>
